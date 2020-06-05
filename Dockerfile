13f42ed0cc645f682c2707c724c122f82ef6db86
ebenyoub <ebenyoub@me.com>
2020-06-05 18:24:15 +0200

    remplace

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM		debian:buster
 LABEL		maintainer="Elyas BENYOUB <ebenyoub@student.le-101.fr>"
 WORKDIR		/ft_server
 COPY		srcs /ft_server
-RUN		/ft_server/server.sh
+RUN			apt-get update && apt-get upgrade -y && apt-get install -y figlet
 EXPOSE		80 443
-CMD		service mysql restart\
-		&& service php7.3-fpm start\
-		&& nginx -g 'daemon off;' 
+CMD         sh server.sh && tail -f /dev/null
--- srcs/server.sh
+++ srcs/server.sh
@@ -1,5 +1,5 @@
 # Installation des paquets
-apt-get update && apt-get upgrade -y
+figlet installation
 apt-get install -y apt-utils
 apt-get install -y openssl
 apt-get install -y nginx
@@ -10,35 +10,38 @@ apt-get install -y libnss3-tools
 apt-get install -y wget
 
 # Configuration de mariadb-server
+figlet mariadb
 service mysql start
 echo "CREATE DATABASE wordpress;" | mysql -u root
-echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'ebenyoub' WITH GRANT OPTION;" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'user'@'localhost' IDENTIFIED BY 'ebenyoub' WITH GRANT OPTION;" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # Configuration Certificat OpenSSL
-
+figlet OpenSSL
 mkdir -p /etc/nginx/ssl
 chmod 700 /etc/nginx/ssl
-chown -R www-data /var/www/*
-chmod -R 755 /var/www/*
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/localhost.key -out /etc/nginx/ssl/localhost.csr -subj "/C=FR/ST=France/L=Lyon/O=ebenyoub/OU=42/CN=localhost"
 
 # Configuration de Nginx
+figlet nginx
 mkdir -p /var/www/ebenyoub
 mv /ft_server/index.html /var/www/ebenyoub
+mv /ft_server/localhost.conf /etc/nginx/sites-available/
+mv /ft_server/localhost_indexoff.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/localhost.conf /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
-mv /ft_server/localhost.conf /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/localhost.conf /etc/nginx/sites-enabled/
 
 # Installation de Wordpress
+figlet wordpress
 wget https://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
-cp -rf wordpress /var/www/ebenyoub
-cp /ft_server/wp-config.php /var/www/ebenyoub/wordpress/
+mv wordpress /var/www/ebenyoub
+mv /ft_server/wp-config.php /var/www/ebenyoub/wordpress/
 rm -rf /var/www/ebenyoub/wordpress/wp-config-sample.php
 
 # Installation de Phpmyadmin
+figlet phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
 tar -zxvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
 mv phpMyAdmin-4.9.0.1-all-languages /var/www/ebenyoub/phpmyadmin
@@ -50,6 +53,7 @@ chmod -R 755 /var/www/*
 chmod 744 /ft_server/index.sh
 
 # Redémarrer les services
+figlet services
 service mysql restart
 /etc/init.d/php*-fpm start
 service nginx restart
